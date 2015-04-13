d63e10af08d4ce8e189085753a658b0fef1aa12f
Fernando Checa <info@fernandocheca.com>
2015-04-13 13:24:42 +0200

    + Added long dynamic and precise fade. You could send a message from VERA with the exact fade you need and how long it should take to get there. 	this is an example message using Lua code: 	luup.call_action("urn:upnp-arduino-cc:serviceId:arduino1", "SendCommand", {radioId="6;100", variableId="DIMMER", value="100,25"}, 130) 	message sent for node id 6, parent node id 130, message "100,25" which means fade to 100% in 25 seconds. + Made fade non exclusive, so in case of long fades, the node can keep working sending/receiving messages.

--- libraries/MySensors/examples/DimmableLEDActuator/DimmableLEDActuator.ino
+++ libraries/MySensors/examples/DimmableLEDActuator/DimmableLEDActuator.ino
@@ -17,23 +17,28 @@
  * REVISION HISTORY
  * Version 1.0 - February 15, 2014 - Bruce Lacey
  * Version 1.1 - August 13, 2014 - Converted to 1.4 (hek) 
- *
+ * Version 1.2 - April 13, 2014 - Added long dynamic and precise fade, also made fade non exclusive (ferpando)
  ***/
+ 
+ #include <Time.h> 
+ 
 #define SN "DimmableLED"
-#define SV "1.1"
+#define SV "1.2"
 
 #include <MySensor.h> 
 #include <SPI.h>
 
-#define LED_PIN 3      // Arduino pin attached to MOSFET Gate pin
-#define FADE_DELAY 10  // Delay in ms for each percentage fade up/down (10ms = 1s full-range dim)
+#define LED_PIN 5      // Arduino pin attached to MOSFET Gate pin
 
-MySensor gw;
+MySensor gw(9,10);
 
 static int currentLevel = 0;  // Current dim level...
+static int requestedLevel = 0;
+
 MyMessage dimmerMsg(0, V_DIMMER);
 MyMessage lightMsg(0, V_LIGHT);
 
+int FADE_DELAY;   // Delay in ms for each percentage fade up/down (10ms = 1s full-range dim)
 
 /***
  * Dimmable LED initialization method
@@ -49,6 +54,7 @@ void setup()
   gw.sendSketchInfo(SN, SV);
   // Pull the gateway's current dim level - restore light level upon sendor node power-up
   gw.request( 0, V_DIMMER );
+
 }
 
 /***
@@ -57,36 +63,56 @@ void setup()
 void loop() 
 {
   gw.process();
+  fadeToLevel();
+  // if delay is too long, it might stop the node from receiving/sending more messages for a long time.
+  //doing it this way, the node can keep functioning regardless fade completition.
 }
-
-
 
 void incomingMessage(const MyMessage &message) {
   if (message.type == V_LIGHT || message.type == V_DIMMER) {
     
-    //  Retrieve the power or dim level from the incoming request message
-    int requestedLevel = atoi( message.data );
-    
-    // Adjust incoming level if this is a V_LIGHT variable update [0 == off, 1 == on]
-    requestedLevel *= ( message.type == V_LIGHT ? 100 : 1 );
-    
-    // Clip incoming level to valid range of 0 to 100
-    requestedLevel = requestedLevel > 100 ? 100 : requestedLevel;
-    requestedLevel = requestedLevel < 0   ? 0   : requestedLevel;
-    
-    Serial.print( "Changing level to " );
-    Serial.print( requestedLevel );
-    Serial.print( ", from " ); 
-    Serial.println( currentLevel );
+    if(message.sensor == 100 ){ //fade special, addressed to node 100
+      // Message payload: value to fade to, seconds to fade     example: "20,10"
+      
+      int commaIndex = msg.indexOf(',');
 
-    fadeToLevel( requestedLevel );
+      String firstValue = msg.substring(0, commaIndex);
+      String secondValue = msg.substring(commaIndex+1);
+
+      requestedLevel=firstValue.toInt();
+      int steps=abs(currentLevel-requestedLevel);
+
+      if(commaIndex == -1){
+         FADE_DELAY=10;
+      }else { 
+         FADE_DELAY = 1000* secondValue.toInt() / steps;
+      }
+    }else { //fade normal
     
-    // Inform the gateway of the current DimmableLED's SwitchPower1 and LoadLevelStatus value...
-    gw.send(lightMsg.set(currentLevel > 0 ? 1 : 0));
-
-    // hek comment: Is this really nessesary?
-    gw.send( dimmerMsg.set(currentLevel) );
-
+          FADE_DELAY=10;
+          //  Retrieve the power or dim level from the incoming request message
+          requestedLevel = atoi( message.data );
+          
+          // Adjust incoming level if this is a V_LIGHT variable update [0 == off, 1 == on]
+          requestedLevel *= ( message.type == V_LIGHT ? 100 : 1 );
+          
+          // Clip incoming level to valid range of 0 to 100
+          requestedLevel = requestedLevel > 100 ? 100 : requestedLevel;
+          requestedLevel = requestedLevel < 0   ? 0   : requestedLevel;
+          
+          }
+       
+      Serial.print( "Changing level to " );
+      Serial.print( requestedLevel );
+      Serial.print( ", from " ); 
+      Serial.println( currentLevel ); 
+       
+         
+      // Inform the gateway of the current DimmableLED's SwitchPower1 and LoadLevelStatus value...
+      gw.send(lightMsg.set(currentLevel > 0 ? 1 : 0));
+  
+      // hek comment: Is this really nessesary?
+      gw.send( dimmerMsg.set(currentLevel) );
     
     }
 }
@@ -94,14 +120,17 @@ void incomingMessage(const MyMessage &message) {
 /***
  *  This method provides a graceful fade up/down effect
  */
-void fadeToLevel( int toLevel ) {
+void fadeToLevel() {
 
-  int delta = ( toLevel - currentLevel ) < 0 ? -1 : 1;
+  if ( currentLevel != requestedLevel ) {
+    int delta = ( requestedLevel - currentLevel ) < 0 ? -1 : 1;
   
-  while ( currentLevel != toLevel ) {
     currentLevel += delta;
+    Serial.print( "analog write: " );
+    Serial.println( currentLevel / 100. * 255 );
     analogWrite( LED_PIN, (int)(currentLevel / 100. * 255) );
     delay( FADE_DELAY );
   }
+  
 }
 
